fd1eb55dec427ce26ea4b282e59bf4594ed7c3b3
Giandomenico Olini <giando.olini@gogodigital.it>
2020-06-06 13:54:51 +0200

    Update version

--- FontAwesomeAsset.php
+++ FontAwesomeAsset.php
@@ -5,9 +5,9 @@
 * @company Gogodigital Srls - Wide ICT Solutions 
 * @website http://www.gogodigital.it
 * @github https://github.com/cinghie/yii2-fontawesome
-* @license GNU GENERAL PUBLIC LICENSE VERSION 3
+* @license BSD-3-Clause
 * @package yii2-fontawesome
-* @version 1.4.2
+* @version 1.4.3
 */
 
 namespace cinghie\fontawesome;
@@ -19,11 +19,9 @@ use yii\web\YiiAsset;
 
 /**
  * Class FontAwesomeAsset
- * @package cinghie\fontawesome
  */
 class FontAwesomeAsset extends AssetBundle
 {
-
     /**
      * @inherit
      */
@@ -44,5 +42,4 @@ class FontAwesomeAsset extends AssetBundle
 	    BootstrapAsset::class,
 	    BootstrapPluginAsset::class
     ];
-
 }
