95d24b0b4c3d7dfdde29e5d0ca6e3ad977f2d071
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 11:53:46 +0200

    Configure Yii bower alias for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,20 @@ declare(strict_types=1);
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 require dirname(__DIR__) . '/vendor/yiisoft/yii2/Yii.php';
+
+$vendorPath = dirname(__DIR__) . '/vendor';
+$bowerCandidates = [
+    $vendorPath . '/bower',
+    $vendorPath . '/bower-asset',
+];
+
+foreach ($bowerCandidates as $bowerPath) {
+    if (is_dir($bowerPath . '/fontawesome')) {
+        Yii::setAlias('@bower', $bowerPath);
+        break;
+    }
+}
+
+if (Yii::getAlias('@bower', false) === false) {
+    throw new RuntimeException('Unable to locate the Composer-installed bower asset directory.');
+}
