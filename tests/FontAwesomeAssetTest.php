454a1c1d2a4cd683e8ef4dab5167703440761146
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-19 11:24:08 +0200

    Add asset integration and webfont tests

--- tests/FontAwesomeAssetTest.php
+++ tests/FontAwesomeAssetTest.php
@@ -8,6 +8,8 @@ use cinghie\fontawesome\FontAwesomeAsset;
 use cinghie\fontawesome\FontAwesomeMinifyAsset;
 use PHPUnit\Framework\TestCase;
 use Yii;
+use yii\web\Application;
+use yii\web\View;
 
 final class FontAwesomeAssetTest extends TestCase
 {
@@ -29,9 +31,70 @@ final class FontAwesomeAssetTest extends TestCase
         self::assertSame([], $asset->depends);
     }
 
-    public function testFontAwesomeCssFilesExist(): void
+    public function testFontAwesomeDistributionFilesExist(): void
     {
         self::assertFileExists(Yii::getAlias('@bower/fontawesome/css/all.css'));
         self::assertFileExists(Yii::getAlias('@bower/fontawesome/css/all.min.css'));
+        self::assertFileExists(Yii::getAlias('@bower/fontawesome/webfonts/fa-brands-400.woff2'));
+        self::assertFileExists(Yii::getAlias('@bower/fontawesome/webfonts/fa-regular-400.woff2'));
+        self::assertFileExists(Yii::getAlias('@bower/fontawesome/webfonts/fa-solid-900.woff2'));
+    }
+
+    public function testAssetBundleRegistersAndPublishesThroughAssetManager(): void
+    {
+        $runtimePath = sys_get_temp_dir() . '/yii2-fontawesome-' . bin2hex(random_bytes(8));
+        $assetPath = $runtimePath . '/assets';
+        mkdir($assetPath, 0777, true);
+
+        $application = new Application([
+            'id' => 'yii2-fontawesome-tests',
+            'basePath' => dirname(__DIR__),
+            'runtimePath' => $runtimePath,
+            'components' => [
+                'assetManager' => [
+                    'basePath' => $assetPath,
+                    'baseUrl' => '/assets',
+                ],
+            ],
+        ]);
+
+        try {
+            $view = new View();
+            $bundle = FontAwesomeAsset::register($view);
+
+            self::assertInstanceOf(FontAwesomeAsset::class, $bundle);
+
+            $bundle->publish($application->assetManager);
+
+            self::assertNotNull($bundle->basePath);
+            self::assertDirectoryExists($bundle->basePath);
+            self::assertFileExists($bundle->basePath . '/css/all.css');
+            self::assertFileExists($bundle->basePath . '/webfonts/fa-solid-900.woff2');
+        } finally {
+            Yii::$app = null;
+            $this->removeDirectory($runtimePath);
+        }
+    }
+
+    private function removeDirectory(string $path): void
+    {
+        if (!is_dir($path)) {
+            return;
+        }
+
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($path, \FilesystemIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($iterator as $item) {
+            if ($item->isDir()) {
+                rmdir($item->getPathname());
+            } else {
+                unlink($item->getPathname());
+            }
+        }
+
+        rmdir($path);
     }
 }
